9965107714d18f1d8a929b247199960c6eda3eb7
Erum shahzadi <163527258+Erumshahzadi@users.noreply.github.com>
2024-04-11 19:34:06 +0500

    Add files via upload
    vacation work (9 to 12 Friday)

---
+++ main.ts
@@ -0,0 +1,40 @@
+// Q8: Write a simple calculator program.
+   
+// Addition
+// Subtraction
+// Multiplication
+// Division
+
+import inquirer from "inquirer";
+
+const answer= await inquirer.prompt([
+
+{ message:"Enter your first number",type: "number", name:"firstNumber"},
+{message :"Enter your second number", type: "number", name: "secondNumber"},
+{
+     message: "Select one operator to perform oprations",
+     type: "list",
+     name: "operator",
+     choices: ["Addition", "Subtraction","Multiplication","Division"],
+},
+
+]);
+
+// conditional statment if-else
+
+if (answer.operator ==="Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator==="Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator ==="Multiplication"){
+    console.log (answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator ==="Division"){
+    console.log (answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("invalid operation");
+}
+
